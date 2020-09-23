300119bf4ee621bda2728d75779810c7c75259d3
Pavel Potapenko <ppa090886@icloud.com>
2020-09-23 13:55:02 +0300

    дз5 сделал итог заказа

--- views/orderOne.php
+++ views/orderOne.php
@@ -5,32 +5,33 @@
 
 $items = json_decode($order->items, true);
 
+$result = 0;
+
 ?>
 
 <div style="padding: 10px; margin: 10px; border: 1px solid #ececec;">
-
     <h1>Заказ № <?= $order->id ?></h1>
-
     <p>Товары:</p>
 
-<?php foreach ($items as $item) : ?>
+    <?php foreach ($items as $item) : ?>
 
-    <?php
-    $total = $item['price'] * $item['count'];
-//    var_dump($total);
-    ?>
+        <?php
+            $total = $item['count'] * $item['price'];
+        ?>
+        <div style="border:1px solid black; padding:20px; margin:10px;">
+            <?= $item['name'] ?><br>
+            Кол-во: <?= $item['count'] ?><br>
+            Стоимость: <?= $total ?> ₽<br>
+        </div>
 
-    <div style="border:1px solid black; padding:20px; margin:10px;">
-        <?= $item['name'] ?><br>
-        Кол-во: <?= $item['count'] ?><br>
-        Стоимость: <?= $item['price'] * $item['count'] ?> ₽<br>
-    </div>
+        <?php
+            $result += $total;
+        ?>
 
-<?php endforeach; ?>
+    <?php endforeach; ?>
+
+    <p>Итого: <?= $result ?> ₽</p>
     <hr>
-
     <a href="?c=order&a=all">Назад</a>
-
     <a href="?c=order&a=del&id=<?= $order->id ?>">Удалить</a>
-
 </div>
